ebe5baa6f1d5eafd400e72f9fce1938ce72a2891
Ardhi Lukianto <ardhi@lukianto.com>
2022-01-25 10:52:56 +0700

    Refactor

--- ndut/init.js
+++ ndut/init.js
@@ -6,11 +6,13 @@ module.exports = async function (options) {
   for (const s of this.ndutDb.schemas) {
     if (['DbjHistory'].includes(s.name)) continue
     let feature = options.model[s.name]
-    if (!feature) feature = _.get(s, 'feature.journal')
+    if (!feature) feature = _.get(s, 'feature.ndutDbJournalModel')
     if (feature && _.isBoolean(feature)) feature = { stateColumn: 'status' }
     if (!feature) continue
     if (feature && _.has(s.properties, feature.stateColumn)) model[s.name] = feature
     else throw new Error(`Model '${s.name}' doesn't have '${feature.stateColumn}' column`)
+    const ds = _.find(this.ndutDb.dataSources, { name: s.dataSource })
+    if (['memory'].includes(ds.connector)) throw new Error(`Model '${s.name}' is bound to a unsupported/memory datasource`)
   }
   this.ndutDbJournal.model = model
   for (const m of Object.keys(model)) {
